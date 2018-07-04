6db57f4f3330917037578140880ff3a9c801a706
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-04 18:07:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+  students_array.each do |x|
+    Student.new(x)
+  end
+end
+      
   end
 
   def add_student_attributes(attributes_hash)
